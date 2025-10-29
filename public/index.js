aa82b52bcb464fd30b7bf35c909227b2eec06fd0
charanjeet1997 <charanjeetjaswani@gmail.com>
2025-10-29 15:34:25 +0530

    added model viewer

--- public/index.js
+++ public/index.js
@@ -10,8 +10,16 @@ fetch('api/products').then(response => response.json()).then(data => {
     });
 
     modelSelector.addEventListener('change', (event) => {
-        const selectedProduct =  data.find(x => x.name === event.target.value);
-        const selectedProductJson =  JSON.stringify(selectedProduct)
+        const selectedJson =  data.find(x => x.name === event.target.value);
+        const selectedProductJson =  JSON.stringify(selectedJson)
+        const selectedProduct = JSON.parse(selectedProductJson);
+        console.log('Received modelSelected event: ', selectedProduct);
+        const selectedModel = selectedProduct.path;
+        const modelViewer = document.getElementById('viewer');
+        modelViewer.src = selectedModel;
+
+        // modelViewer.setAttribute('min-camera-orbit', `auto 90deg ${selectedProduct.zoom}`);
+        // modelViewer.setAttribute('max-camera-orbit', `auto 90deg ${selectedProduct.zoom}`);
         console.log(`Model selected: ${selectedProductJson}`);
         socket.emit('modelSelected', { selectedProduct: selectedProductJson });
     });
