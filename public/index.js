0f25d6ea6055eb27d446a809fed4d45b194a9406
charanjeet1997 <charanjeetjaswani@gmail.com>
2025-10-30 10:54:06 +0530

    solving page reload client also upate problem

--- public/index.js
+++ public/index.js
@@ -2,29 +2,33 @@
 
 fetch('api/products').then(response => response.json()).then(data => {
     const modelSelector = document.getElementById('modelSelector');
+    const modelViewer = document.getElementById('viewer');
     data.forEach(product => {
         const option = document.createElement('option');
         option.value = product.name;
         option.text = product.name;
         modelSelector.appendChild(option);
     });
+    SelectProduct(data[0].name);
 
     modelSelector.addEventListener('change', (event) => {
-        const selectedJson =  data.find(x => x.name === event.target.value);
+        SelectProduct(event.target.value);
+    });
+
+    function SelectProduct(value)
+    {
+        const selectedJson =  data.find(x => x.name === value);
         const selectedProductJson =  JSON.stringify(selectedJson)
         const selectedProduct = JSON.parse(selectedProductJson);
         console.log('Received modelSelected event: ', selectedProduct);
         const selectedModel = selectedProduct.path;
-        const modelViewer = document.getElementById('viewer');
         modelViewer.src = selectedModel;
 
         // modelViewer.setAttribute('min-camera-orbit', `auto 90deg ${selectedProduct.zoom}`);
         // modelViewer.setAttribute('max-camera-orbit', `auto 90deg ${selectedProduct.zoom}`);
         console.log(`Model selected: ${selectedProductJson}`);
         socket.emit('modelSelected', { selectedProduct: selectedProductJson });
-
-    });
-    const modelViewer = document.getElementById('viewer');
+    }
     modelViewer.addEventListener('camera-change', () => {
         const {theta} = modelViewer.getCameraOrbit();
         const degree = theta * (180 / Math.PI);
